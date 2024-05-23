6543fd3a71df75ee8621dd0e81821f73cf7bbcf9
youjun-lee <youjun.lee@toss.im>
2024-05-23 16:52:49 +0900

    android test done

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Alert, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import {ConvertUrl} from "@tosspayments/widget-sdk-react-native/src/utils/convertUrl"
 const HomeScreen = ({ navigation }) => {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState('http://52.78.198.128/sample/');
 
   return (
     <View style={styles.container}>
@@ -25,8 +25,39 @@ const HomeScreen = ({ navigation }) => {
 
 const WebViewScreen = ({ route }) => {
   const { url } = route.params;
-  return <WebView source={{ uri: url }} style={{ flex: 1 }} />;
-};
+  console.log('URL >>:', url);
+
+  const handleInvalidUrl = (invalidUrl) => {
+    // 유효하지 않은 URL에 대한 처리
+    Alert.alert('Invalid URL', `${invalidUrl} is not a valid URL. The URL does not start with http or https.`);
+  };
+
+  const handleShouldStartLoadWithRequest = (navState) => {
+    const { url: newUrl } = navState;
+    console.log('areyou here?')
+    if (newUrl.startsWith('http://') || newUrl.startsWith('https://')) {
+      console.log('URL starts with http or https:', newUrl);
+      return true; // URL이 유효한 경우 true 반환
+    }else if (newUrl.startsWith('intent://')) {
+      console.log('Handling intent URL:', newUrl);
+      urlConverter(newUrl)
+      return false; // intent URL은 WebView에서 로드하지 않음
+    } else {
+      console.log('URL does not start with http or https:', newUrl);
+      handleInvalidUrl(newUrl); // 유효하지 않은 URL에 대한 함수 호출
+      return false;
+    }
+  };
+
+
+  return (
+    <WebView
+      source={{ uri: url }}
+      style={{ flex: 1 }}
+      onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest}
+      originWhitelist={["*"]}
+    />
+  );};
 
 const Stack = createStackNavigator();
 
@@ -41,6 +72,19 @@ function App() {
   );
 }
 
+const urlConverter = (url: string) => {
+  const convertUrl = new ConvertUrl(url);
+  if(convertUrl.isAppLink()){
+    convertUrl.launchApp().then((isLaunch) => {
+      if (isLaunch === false) { 
+          // 앱 실행 실패시 처리 로직
+      }
+    });
+  
+  }else{
+    return true;
+  };
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
